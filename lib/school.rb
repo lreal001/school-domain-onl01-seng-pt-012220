a86615674d7f5c348af9d0717c1d32f0464b0082
Lessllie Real <rlessllie@yahoo.com>
2020-02-21 05:38:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_reader :roster, :name 
+ 
+  def initialize(name)
+    i = 0
+     roster = {}
+    while i < roster.length 
+    students_names << @name 
+    end 
+  
+  
+   def add_student(name, grade)
+     roster << students_names
+end 
+
+  
+
+
+end 
+
+end 
